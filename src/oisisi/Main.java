136e95f833098fdf1a5ce8b9662b6ba43a8c1f5c
piwneuh <filip.pinjuh@gmail.com>
2020-11-26 13:53:28 +0100

    E napravio sam mejn :D
    Uspesno

---
+++ src/oisisi/Main.java
@@ -0,0 +1,13 @@
+package oisisi;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new MainFrame();
+		frame.setVisible(true);
+
+	}
+
+}
