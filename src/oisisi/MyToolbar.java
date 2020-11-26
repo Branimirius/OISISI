aadfec94351aa0a36f7bfdfc05dc5ee0ddc06008
piwneuh <filip.pinjuh@gmail.com>
2020-11-26 17:13:24 +0100

    #ToolBar (smanjiti jtxtfield)
    jtextfield je veci nego sto treba al to je to
    dodat furnance flavour

--- src/oisisi/MyToolbar.java
+++ src/oisisi/MyToolbar.java
@@ -1,7 +1,10 @@
 package oisisi;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTextField;
@@ -21,26 +24,38 @@ public class MyToolbar extends JToolBar {
 		
 		JButton btnNew = new JButton(new ImageIcon("images_mc/crafting_table.png"));
 		btnNew.setToolTipText("New");
+		btnNew.setBorder(BorderFactory.createEmptyBorder());
 		add(btnNew);
 		
 		addSeparator();
 				
 		JButton btnEdit = new JButton(new ImageIcon("images_mc/anvil.png"));
 		btnEdit.setToolTipText("Edit");
+		btnEdit.setBorder(BorderFactory.createEmptyBorder());
 		add(btnEdit);
 		
 		addSeparator();
 		
-		JButton btnDelete = new JButton(new ImageIcon("images_mc/furnance.png"));
+		JButton btnDelete = new JButton(new ImageIcon("images_mc/unlit_furnance.png"));
 		btnDelete.setToolTipText("Delete");
+		btnDelete.setBorder(BorderFactory.createEmptyBorder());
+		btnDelete.setRolloverIcon(new ImageIcon("images_mc/furnance.png"));
+		btnDelete.setRolloverEnabled(true);
 		add(btnDelete);
 		
 		addSeparator();
 		
-		JButton btnRight=new JButton("search");
-		add(btnRight,BorderLayout.EAST);
-		
 		setFloatable(false);
 		
+		add(Box.createHorizontalGlue());
+		//add(new JToolBar.Separator(new Dimension(15,10)));
+		
+		JTextField field = new JTextField();
+		add(field);
+		
+		JButton btnSearch=new JButton(new ImageIcon("images_mc/search.png"));
+		btnSearch.setToolTipText("Search");
+		btnSearch.setBorder(BorderFactory.createEmptyBorder());
+		add(btnSearch);
 	}
 }
