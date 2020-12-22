04f555c5aaa3d53adb3378f31e4544487feccf77
Branimirius <73712742+Branimirius@users.noreply.github.com>
2020-12-22 19:36:01 +0100

    #student
    Model studenta

---
+++ src/model/Student.java
@@ -0,0 +1,124 @@
+package model;
+
+enum status {B,S};
+
+public class Student {
+	private String prezime;
+	private String ime;
+	private String datumRodjenja;
+	private String adresaStana;
+	private String kontaktTel;
+	private String eMail;
+	private String brIndeksa;
+	private Integer godUpisa;
+	private Integer godStudija;
+	private status statusStudenta;
+	private double prosecnaOcena;
+	
+	public Student(String prezime, String ime, String datumRodjenja, String adresaStana, String kontaktTel,
+			String eMail, String brIndeksa, Integer godUpisa, Integer godStudija, status statusStudenta,
+			double prosecnaOcena) {
+		super();
+		this.prezime = prezime;
+		this.ime = ime;
+		this.datumRodjenja = datumRodjenja;
+		this.adresaStana = adresaStana;
+		this.kontaktTel = kontaktTel;
+		this.eMail = eMail;
+		this.brIndeksa = brIndeksa;
+		this.godUpisa = godUpisa;
+		this.godStudija = godStudija;
+		this.statusStudenta = statusStudenta;
+		this.prosecnaOcena = prosecnaOcena;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getDatumRodjenja() {
+		return datumRodjenja;
+	}
+
+	public void setDatumRodjenja(String datumRodjenja) {
+		this.datumRodjenja = datumRodjenja;
+	}
+
+	public String getAdresaStana() {
+		return adresaStana;
+	}
+
+	public void setAdresaStana(String adresaStana) {
+		this.adresaStana = adresaStana;
+	}
+
+	public String getKontaktTel() {
+		return kontaktTel;
+	}
+
+	public void setKontaktTel(String kontaktTel) {
+		this.kontaktTel = kontaktTel;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getBrIndeksa() {
+		return brIndeksa;
+	}
+
+	public void setBrIndeksa(String brIndeksa) {
+		this.brIndeksa = brIndeksa;
+	}
+
+	public Integer getGodUpisa() {
+		return godUpisa;
+	}
+
+	public void setGodUpisa(Integer godUpisa) {
+		this.godUpisa = godUpisa;
+	}
+
+	public Integer getGodStudija() {
+		return godStudija;
+	}
+
+	public void setGodStudija(Integer godStudija) {
+		this.godStudija = godStudija;
+	}
+
+	public status getStatusStudenta() {
+		return statusStudenta;
+	}
+
+	public void setStatusStudenta(status statusStudenta) {
+		this.statusStudenta = statusStudenta;
+	}
+
+	public double getProsecnaOcena() {
+		return prosecnaOcena;
+	}
+
+	public void setProsecnaOcena(double prosecnaOcena) {
+		this.prosecnaOcena = prosecnaOcena;
+	}
+	
+	
+}
