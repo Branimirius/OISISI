3cf76f17d74c172579a4f493f259b362aa54d501
piwneuh <filip.pinjuh@gmail.com>
2020-12-22 18:05:13 +0100

    #profesor #predmet
    napravio sam paket:model  pa tu stavljaj classe student i ocene

---
+++ src/model/Profesor.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.List;
+
+public class Profesor {
+
+     private String prezime;
+	 private String ime;
+	 private Integer datum_rodjenja;
+     private String adresa_stanovanja;
+     private Integer kontakt_telefon;
+     private String email_adresa;
+     private Integer broj_licne_karte;
+     private String titula;
+     private String zvanje;
+     private List<Predmet> lista_predmeta;
+	
+}
